61defc92a435508e138159505284bcdc47f579c6
Aleksei KUROCHKIN <akurochk@paul-f5Ar2s5.clusters.42paris.fr>
2025-02-07 19:47:36 +0100

    add: QUIT

--- includes/Replyes.hpp
+++ includes/Replyes.hpp
@@ -6,7 +6,7 @@
 /*   By: akurochk <akurochk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 12:28:11 by akurochk          #+#    #+#             */
-/*   Updated: 2025/02/07 12:35:11 by akurochk         ###   ########.fr       */
+/*   Updated: 2025/02/07 18:37:02 by akurochk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 # define CRLF			"\r\n"
 # define CMD_LEN		512
 # define SERVER_NAME	"DarkForest"
-# define CHANNEL_LIMIT	3
 
 // client = nickname of user
 
 // Mode was changed (only if changed)
 # define MODE_SET(client, channelName, mode, variable)				std::string(":") + (client.empty() ? "empty" : client) + " MODE " + channelName + " " + mode + " " + variable + ""
-
 # define JOIN_OK(client, userName, hostName, channelName)			std::string(":") + (client.empty() ? "empty" : client) + "!" + userName + "@" + hostName + " JOIN :" + channelName + ""
+# define QUIT_CHANNEL(client, userName, hostName, msg)				std::string(":") + (client.empty() ? "empty" : client) + "!" + userName + "@" + hostName + " QUIT :" + msg + ""
+# define QUIT_SERVER(client, msg)									"ERROR :Closing Link: " + (client.empty() ? "empty" : client) + " " + msg + ""
 
 // --- errors ---
 // "401" // WHOIS PRIVMSG
