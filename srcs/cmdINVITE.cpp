cd07a95c2443f6e4b42418ec53007f7387fc4a4d
Masha Klimina <43732011+vecherinca@users.noreply.github.com>
2025-02-10 18:41:26 +0100

    Cmd invite (#23)
    * feat: invite is done
    
    ---------
    
    Co-authored-by: Mariia KLIMINA <mklimina@paul-f4Ar7s10.clusters.42paris.fr>
    Co-authored-by: Mariia KLIMINA <mklimina@paul-f4Ar7s6.clusters.42paris.fr>

--- srcs/cmdINVITE.cpp
+++ srcs/cmdINVITE.cpp
@@ -6,12 +6,13 @@
 /*   By: akurochk <akurochk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 18:11:03 by akurochk          #+#    #+#             */
-/*   Updated: 2025/02/08 12:21:08 by akurochk         ###   ########.fr       */
+/*   Updated: 2025/01/30 18:30:23 by akurochk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 /*
+
 INVITE message
      Command: INVITE
   Parameters: <nickname> <channel>
@@ -48,7 +49,60 @@ IRCv3 invite-notify Extension
 Invite list
 Servers MAY allow the INVITE with no parameter, and reply with a list of channels the sender is invited to as RPL_INVITELIST (336) numerics, ending with a RPL_ENDOFINVITELIST (337) numeric.
 */
-void	Server::cmdInvite(std::vector<std::string> tokens, int fd) {
-	(void) tokens;
-	(void) fd;
+
+void Server::cmdInvite(std::vector<std::string> tokens, int fd)
+{
+
+  std::string channelName;
+  std::string userName;
+  Channel *channel;
+  userName = tokens[1];
+  channelName = tokens[2];
+  Client *c = getClientByFd(fd);
+  Client *invitedUser = getClientByNick(userName);
+  channel = getChannel(channelName);
+
+  if (tokens.size() < 2)
+  {
+    c->setReplyBuffer(ERR_NEEDMOREPARAMS(c->getUsername(), tokens[0]));
+    return;
+  }
+  if (channel == NULL)
+  {
+    c->setReplyBuffer(ERR_NOSUCHCHANNEL(c->getNickname(), channelName));
+    return;
+  }
+  if (!invitedUser)
+  {
+    c->setReplyBuffer(ERR_NOSUCHNICK(c->getNickname(), userName));
+    return;
+  }
+  if (invitedUser->getRegistred() == false) {
+		invitedUser->setReplyBuffer(ERR_NOTREGISTERED(invitedUser->getNickname()));
+		return ;
+	}
+  if (channel->getClientByFd(c->getFd()) == NULL)
+  {
+    c->setReplyBuffer(ERR_NOTONCHANNEL(c->getNickname(), channelName));
+    return;
+  }
+  if (channel->getClientByFd(invitedUser->getFd()))
+  {
+    c->setReplyBuffer(ERR_USERONCHANNEL(c->getNickname(), invitedUser->getNickname(), channelName));
+    return;
+  }
+  if (channel->getI() == true && channel->getOperatorByFd(c->getFd()) == NULL)
+  {
+    c->setReplyBuffer(ERR_CHANOPRIVSNEEDED(c->getNickname(), channelName));
+    return;
+  }
+  if (invitedUser->isInvitedToChannel(channelName))
+    return;
+
+  invitedUser->addInvite(channelName);
+  c->setReplyBuffer(RPL_INVITING(c->getNickname(), userName, channelName));
+
+  std::stringstream ss;
+  ss << c->getNickname() << " INVITE " << userName << " " << channelName;
+  invitedUser->setReplyBuffer(ss.str());
 }
