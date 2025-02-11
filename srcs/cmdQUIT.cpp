24ab7d6574d4fb0d79fdc77bd99795646999f940
Aleksei KUROCHKIN <akurochk@bess-f2r1s18.clusters.42paris.fr>
2025-02-11 17:30:53 +0100

    fix: TOPIC

--- srcs/cmdQUIT.cpp
+++ srcs/cmdQUIT.cpp
@@ -6,7 +6,7 @@
 /*   By: akurochk <akurochk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 18:11:14 by akurochk          #+#    #+#             */
-/*   Updated: 2025/02/10 14:35:50 by akurochk         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:20:07 by akurochk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ void	Server::cmdQuit(std::vector<std::string> tokens, int fd) {
 	if (tokens.size() > 1) {
 		if (tokens[1][0] == ':') {
 			tokens[1].erase(tokens[1].begin());
-		}
-		for (size_t i = 1; i < tokens.size(); i++) {
-			msg += " " + tokens[i];
+			for (size_t i = 1; i < tokens.size(); i++) {
+				msg += " " + tokens[i];
+			}
+		} else {
+			msg += " " + tokens[1];
 		}
 	}
 
--- srcs/cmdTOPIC.cpp
+++ srcs/cmdTOPIC.cpp
@@ -6,73 +6,84 @@
 /*   By: akurochk <akurochk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 18:11:17 by akurochk          #+#    #+#             */
-/*   Updated: 2025/01/30 18:31:05 by akurochk         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:15:35 by akurochk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-void Server::cmdTopic(std::vector<std::string> tokens, int fd)
-{
-    std::string channelName = tokens[1];
-    std::string comment;
-    Client *c = getClientByFd(fd);
+// === in irssi documentation ===
+// TOPIC [-delete]	[<channel>]		[:<topic>]								// syntax of irssi (all 3 elem -> set topic)
+// TOPIC 			[#ch]													// show topic of #ch
+// TOPIC 			[#ch] 			[:The] [robots] [are] [taking] [over!]	// set topic of #ch 
+// TOPIC [-delete]	[#ch]													// delete topic of #ch 
+// TOPIC 			[#shakespeare]	[:/bb|[^b]{2}/]							// set topic 
 
-    if (tokens.size() < 2)
-    {
-        c->setReplyBuffer(ERR_NEEDMOREPARAMS(c->getNickname(), tokens[0]));
-        return;
-    }
+void Server::cmdTopic(std::vector<std::string> tokens, int fd) {
+	
+	std::string	comment;
+	Channel		*ch;
+	Client		*c = getClientByFd(fd);
 
-    if (tokens.size() > 2)
-    {
-        for (size_t i = 2; i < tokens.size(); ++i)
-        {
-            if (i != 2)
-            {
-                comment += " ";
-            }
-            comment += tokens[i];
-        }
-    }
+	if (c->getRegistred() == false) {
+		c->setReplyBuffer(ERR_NOTREGISTERED(c->getNickname()));
+		return ;
+	}
 
-    Channel *channel = getChannel(channelName);
-    if (!channel)
-    {
-        c->setReplyBuffer(ERR_NOSUCHCHANNEL(c->getNickname(), channelName));
-        return;
-    }
+	if (tokens.size() < 2) {
+		c->setReplyBuffer(ERR_NEEDMOREPARAMS(c->getNickname(), tokens[0]));
+		return ;
+	}
 
-    if (!channel->getClientByFd(c->getFd()))
-    {
-        c->setReplyBuffer(ERR_NOTONCHANNEL(c->getNickname(), channelName));
-        return;
-    }
+	ch = getChannel(tokens[1]);
 
-    if (channel->getT() && channel->getOperatorByFd(c->getFd()) == NULL)
-    {
-        c->setReplyBuffer(ERR_CHANOPRIVSNEEDED(c->getNickname(), channelName));
-        return;
-    }
+	if (ch == NULL) {
+		c->setReplyBuffer(ERR_NOSUCHCHANNEL(c->getNickname(), tokens[1]));
+		return ;
+	}
 
-    if (tokens.size() > 2)
-    {
-        channel->setTopicText(comment);
+	if (ch->getClientByFd(fd) == NULL) {
+		c->setReplyBuffer(ERR_NOTONCHANNEL(c->getNickname(), ch->getName()));
+		return ;
+	}
 
-        std::stringstream ss;
-        ss << c->getUserID() << " TOPIC " << channelName << " : " << comment;
-        channel->setReplyBufferForAllChannelClients(ss.str());
+	// get topic
+	if (tokens.size() == 2) {
+		if (ch->getTopicText().empty() == true) {
+			c->setReplyBuffer(RPL_NOTOPIC(c->getNickname(), ch->getName()));
+		} else {
+			c->setReplyBuffer(RPL_TOPIC(c->getNickname(), ch->getName(), ch->getTopicText()));
+		}
+		return ;
+	}
 
-    }	
-    else  
-    {
-        if (!channel->getTopicText().empty())
-        {
-            c->setReplyBuffer(RPL_TOPIC(c->getNickname(), channelName, channel->getTopicText()));
-        }
-        else
-        {
-            c->setReplyBuffer(RPL_NOTOPIC(c->getNickname(), channelName));
-        }
-    }
+	// if topic protected not operators will be stopped here
+	if (ch->getT() == true && ch->getOperatorByFd(fd) == NULL ) {
+		c->setReplyBuffer(ERR_CHANOPRIVSNEEDED(c->getNickname(), ch->getName()));
+		return;
+	}
+
+	// delete topic
+	if (tokens.size() == 3 && tokens[2] == ":") {
+		ch->setTopicText("");
+		ch->setReplyBufferForAllChannelClients(TOPIC(c->getNickname(), c->getUsername(), c->getHostname(), ch->getName(), ""));
+		return;
+	}
+
+	// set topic
+	std::string	topic = "";
+	if (tokens[2][0] == ':') {
+		tokens[2].erase(tokens[2].begin());
+		for (size_t i = 2; i < tokens.size(); i++) {
+			topic += tokens[i];
+			if (i < tokens.size() - 1) {
+				topic += " ";
+			}
+		}
+	} else {
+		topic += tokens[2];	
+	}
+	
+	ch->setTopicText(topic);
+	ch->setReplyBufferForAllChannelClients(TOPIC(c->getNickname(), c->getUsername(), c->getHostname(), ch->getName(), topic));
 }
