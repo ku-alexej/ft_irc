6c4565fdd7c25f4a1adef6bf3ccb25435bddb4c0
Masha Klimina <43732011+vecherinca@users.noreply.github.com>
2025-02-11 20:13:45 +0000

    feat: working but small bugs

--- srcs/cmdPRIVMSG.cpp
+++ srcs/cmdPRIVMSG.cpp
@@ -4,68 +4,83 @@ void Server::cmdPrivmsg(std::vector<std::string> tokens, int fd)
 {
     Client *c = getClientByFd(fd);
 
+    if (tokens.size() == 1)
+    {
+        c->setReplyBuffer(ERR_NORECIPIENT(c->getNickname(), tokens[0]));
+        return;
+    }
     if (tokens.size() < 3)
     {
-        c->setReplyBuffer(ERR_NOTEXTTOSEND(c->getNickname())); 
+        c->setReplyBuffer(ERR_NOTEXTTOSEND(c->getNickname()));
         return;
     }
 
     std::string message;
-    for (size_t i = 2; i < tokens.size(); ++i)
+    if (tokens[2][0] == ':')
     {
-        if (i != 2)
+        tokens[2].erase(tokens[2].begin());
+        for (size_t i = 2; i < tokens.size(); ++i)
         {
-            message += " ";  // Add space between tokens
+            if (i != 2)
+            {
+                message += " ";
+            }
+            message += tokens[i];
         }
-        message += tokens[i];  // Concatenate the message parts
-    }
-
-    std::vector<std::string> targets = split(tokens[1], ',');
-
-    for (size_t i = 0; i < targets.size(); ++i)
-    {
-        std::string target = targets[i];
-
-        if (target[0] == '#')
+        std::vector<std::string> targets = split(tokens[1], ',');
+       for (size_t i = 0; i < targets.size(); ++i)
         {
-            Channel *channel = getChannel(target); 
-
-            if (!channel)
-            {
-                c->setReplyBuffer(ERR_NOSUCHCHANNEL(c->getNickname(), target)); 
-                continue;  
-            }
+            std::string target = targets[i];
             
-            if (!channel->getClientByFd(c->getFd()))
+            if (target.find('#') != std::string::npos)
             {
-                c->setReplyBuffer(ERR_NOTONCHANNEL(c->getNickname(), target)); // Error if not a member
-                continue;
-            }
+                bool operators = false;
 
-            if (channel->getT() && !channel->getOperatorByFd(c->getFd()))
+                for (std::string::iterator it = target.begin(); it != target.end() && *it != '#'; ++it)
+                {
+                    if (*it == '@')
+                    {
+                        operators = true;
+                        break;
+                    }
+                }
+                
+                size_t pos = target.find('#');
+                std::string channelName = target.substr(pos);
+                Channel *channel = getChannel(channelName);
+                if (!channel)
+                {
+                    c->setReplyBuffer(ERR_NOSUCHCHANNEL(c->getNickname(), channelName));
+                    continue;
+                }
+                else if (!channel->getClientByFd(c->getFd()))
+                {
+                    c->setReplyBuffer(ERR_NOTONCHANNEL(c->getNickname(), channelName));
+                    continue;
+                }
+                
+                std::stringstream ss;
+                ss << c ->getUserID() << " PRIVMSG " << channel -> getName() << " :" << message;
+                
+                if (operators)
+                    continue;
+                else
+                    channel->setReplyBufferForAllChannelClients(ss.str());
+            }
+            else
             {
-                c->setReplyBuffer(ERR_CANNOTSENDTOCHAN(c->getNickname(), target)); // Error if not an operator in a moderated channel
-                continue;
+                Client *recipient = getClientByNick(target); 
+
+                if (!recipient)
+                {
+                    c->setReplyBuffer(ERR_NOSUCHNICK(c->getNickname(), target)); 
+                    continue;  
+                }
+
+                std::stringstream ss;
+                ss << c->getNickname() << " PRIVMSG " << recipient -> getNickname() << " :" << message;
+                recipient->setReplyBuffer(ss.str()); 
             }
-
-            std::stringstream ss;
-            ss << c->getNickname() << " PRIVMSG " << target << " :" << message << CRLF;
-            channel->setReplyBufferForAllChannelClients(ss.str());  // Send the message to all channel clients
-        }
-        else  
-        {
-            // Client *recipient = getClientByNick(target); // Find the client by nickname
-            
-            // if (!recipient)
-            // {
-            //     c->setReplyBuffer(ERR_NOSUCHNICK(c->getNickname(), target)); // Error if user doesn't exist
-            //     continue;  // Move to the next target
-            // }
-
-            // // Send the message to the recipient
-            // std::stringstream ss;
-            // ss << c->getNickname() << " PRIVMSG " << target << " :" << message << CRLF;
-            // recipient->addToWriteBuffer(ss.str()); // Send the message to the recipient
         }
     }
 }
