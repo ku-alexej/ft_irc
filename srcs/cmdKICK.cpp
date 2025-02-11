042c2e60aa257324abc7cefd3129a68e2843a553
Aleksei Kurochkin <94857033+ku-alexej@users.noreply.github.com>
2025-02-11 21:21:48 +0100

    fix KICK (#31)
    * fix: KICK errors
    
    * fix: removed some debug lines
    
    ---------
    
    Co-authored-by: Aleksei KUROCHKIN <akurochk@paul-f4Ar5s10.clusters.42paris.fr>

--- srcs/cmdKICK.cpp
+++ srcs/cmdKICK.cpp
@@ -6,82 +6,92 @@
 /*   By: akurochk <akurochk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 18:11:08 by akurochk          #+#    #+#             */
-/*   Updated: 2025/01/30 18:30:42 by akurochk         ###   ########.fr       */
+/*   Updated: 2025/02/11 21:18:55 by akurochk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
+void	Server::cmdKick(std::vector<std::string> tokens, int fd) {
 
-void Server::cmdKick(std::vector<std::string> tokens, int fd)
-{
-	std::string channelName = tokens[1];
-	std::string comment =  "Very boring and speaks about frogs all the time.";
-	Client *c = getClientByFd(fd);
+	Client						*c = getClientByFd(fd);
+	Channel						*ch;
+	std::string					reason = "";
+	std::string					defaultReason = "Very boring and speaks about frogs all the time.";
+	std::vector<std::string>	listToKick;
 
-	if (tokens.size() < 3)
-	{
+	if (c->getRegistred() == false) {
+		c->setReplyBuffer(ERR_NOTREGISTERED(c->getNickname()));
+		return ;
+	}
+
+	if (tokens.size() < 3) {
 		c->setReplyBuffer(ERR_NEEDMOREPARAMS(c->getNickname(), tokens[0]));
 		return;
 	}
-	if (tokens.size() > 3)
-	{
-		comment.clear();
-		for (size_t i = 3; i < tokens.size(); ++i)
-		{
-			if (i != 2)
-			{
-				comment += " ";
+
+	//prepare reason
+	if (tokens.size() > 3) {
+		if (tokens[3][0] == ':') {
+			tokens[3].erase(tokens[3].begin());
+			for (size_t i = 3; i < tokens.size(); i++) {
+				reason += tokens[i];
+				if (i < tokens.size() - 1) {
+					reason += " ";
+				}
 			}
-			comment += tokens[i];
+		} else {
+			reason += tokens[3];
 		}
 	}
-	std::cout << "COMMENT" << comment << std::endl;
-	Channel *channel = getChannel(channelName);
-	if (!channel)
-	{
-		c->setReplyBuffer(ERR_NOSUCHCHANNEL(c->getNickname(), channelName));
+	if (reason.empty() == true) {
+		reason = defaultReason;
+	}
+
+	ch = getChannel(tokens[1]);
+	if (ch == NULL) {
+		c->setReplyBuffer(ERR_NOSUCHCHANNEL(c->getNickname(), tokens[1]));
 		return;
 	}
 
-	if (!channel->getClientByFd(c->getFd()))
-	{
-		c->setReplyBuffer(ERR_NOTONCHANNEL(c->getNickname(), channelName));
+	if (ch->getClientByFd(c->getFd()) == NULL) {
+		c->setReplyBuffer(ERR_NOTONCHANNEL(c->getNickname(), ch->getName()));
 		return;
 	}
 
-	if (channel->getOperatorByFd(c->getFd()) == NULL)
-	{
-		c->setReplyBuffer(ERR_CHANOPRIVSNEEDED(c->getNickname(), channelName));
+	if (ch->getOperatorByFd(c->getFd()) == NULL) {
+		c->setReplyBuffer(ERR_CHANOPRIVSNEEDED(c->getNickname(), ch->getName()));
 		return;
 	}
 
-	std::vector<std::string> usersToKick = split(tokens[2], ',');
+	listToKick = split(tokens[2], ',');
 
-	for (size_t i = 0; i < usersToKick.size(); ++i)
-	{
-		std::string userName = usersToKick[i];
-		Client *userToKick = getClientByNick(userName);
+	for (size_t i = 0; i < listToKick.size(); i++) {
 
-		if (!userToKick)
-		{
-			c->setReplyBuffer(ERR_NOSUCHNICK(c->getNickname(), userName));
+		Client	*t = getClientByNick(listToKick[i]);
+
+		if (t == NULL) {
+			c->setReplyBuffer(ERR_USERNOTINCHANNEL(c->getNickname(), "*", ch->getName()));
 			continue;
 		}
 
-		if (!channel->getClientByFd(userToKick->getFd()))
-		{
-			c->setReplyBuffer(ERR_USERNOTINCHANNEL(c->getNickname(), userName, channelName));
+		if (t->getNickname() == "") {
+			c->setReplyBuffer(ERR_USERNOTINCHANNEL(c->getNickname(), "*", ch->getName()));
 			continue;
 		}
 
-		std::stringstream ss;
-		ss << c->getUserID() << " KICK " << channelName << " " << userName << " :" << comment;
-		channel->deleteClient(userToKick);
-		channel -> deleteOperator(userToKick);
-		userToKick -> deleteChannel(channelName);
-		channel->setReplyBufferForAllChannelClients(ss.str());
-		userToKick->setReplyBuffer(ss.str());
-		deleteEmptyChannel(channel);
+		if (ch->getClientByFd(t->getFd()) == NULL) {
+			c->setReplyBuffer(ERR_USERNOTINCHANNEL(c->getNickname(), t->getNickname(), ch->getName()));
+			continue;
+		}
+
+		ch->deleteClient(t);
+		ch->deleteOperator(t);
+		t->deleteChannel(ch->getName());
+
+		ch->setReplyBufferForAllChannelClients(KICK(c->getNickname(), c->getUsername(), c->getHostname(), ch->getName(), t->getNickname(), reason));
+		t->setReplyBuffer(KICK(c->getNickname(), c->getUsername(), c->getHostname(), ch->getName(), t->getNickname(), reason));
+
+		deleteEmptyChannel(ch);
 	}
 }
